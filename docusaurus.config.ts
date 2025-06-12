56b6196797d721ef90e517185db8bfeb0bec8021
Maxime Servais <maximeservais@hotmail.com>
2025-06-12 17:21:28 +0200

    adding posthog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,7 +37,6 @@ const config: Config = {
     defaultLocale: "en",
     locales: ["en"],
   },
-
   presets: [
     [
       "classic",
@@ -65,7 +64,16 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
-
+  plugins: [
+    [
+      "posthog-docusaurus",
+      {
+        apiKey: "phc_yIxOHwCOu1TnJZfNoiwJl6mwxc4PuQG8UGkG2VcZEjL",
+        appUrl: "https://us.i.posthog.com",
+        enableInDevelopment: false, // optional
+      },
+    ],
+  ],
   themeConfig: {
     image: "img/docusaurus-social-card.jpg", // TODO: change to uncap OG image
     navbar: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
-
 import styles from "./index.module.css";
 
 function HomepageHeader() {
@@ -29,6 +28,7 @@ function HomepageHeader() {
 
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <Layout
       title={`Welcome to ${siteConfig.title}`}
